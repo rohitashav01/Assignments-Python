74e2cc332a52d752d41d9b7e846dbb3765a5def1
rohitashav01 <rohitashav.pathak@netsolutions.com>
2023-02-14 16:18:35 +0530

    Used __init__ and __main__

--- oops_classes.py
+++ oops_classes.py
@@ -3,114 +3,111 @@
 
 #Class1
 class Vehicle:
-    doors=5
-    wheels=4
-    distance=0
-    speed=0
+    def __init__(self,doors,wheels,distance,speed):
+        self.doors=doors
+        self.wheels=wheels
+        self.distance=distance
+        self.speed=speed
     def car(self):
-        print("Car with four wheels and five doors is jimny")
+        print(f"Car with {self.wheels}  wheels and {self.doors} doors is jimny")
     #Calculating Speed of Vehicle
     def time(self):
         time_taken=self.distance//self.speed
         return time_taken
-    
-model=Vehicle()
-print("Accessing data members using object: ")
-print("No. of wheels in the car are: ",model.wheels)
-print("No. of doors in the car are: ",model.doors)
-print("Accessing function using object: ")
-model.car()
-model.distance=240
-model.speed=40
-print("Speed of car in km/hrs: ",model.speed)
-print("Distance covered by car in kms: ",model.distance)
-print("Time taken by car in hrs: ",model.time())
+if __name__=='__main__':  
+    model=Vehicle(5,4,240,40)
+    print("Accessing data members using object: ")
+    print("No. of wheels in the car are: ",model.wheels)
+    print("No. of doors in the car are: ",model.doors)
+    print("Accessing function using object: ")
+    model.car()
+    print("Speed of car in km/hrs: ",model.speed)
+    print("Distance covered by car in kms: ",model.distance)
+    print("Time taken by car in hrs: ",model.time())
 print("--------------------------------------------------------------------------------------------------------")
 
 
 #Class2
 class Employee:
-    e_id=0
-    e_name=''
-    e_dept=''
+    def __init__(self,e_id,e_name,e_dept):
+        self.e_id=e_id
+        self.e_name=e_name
+        self.e_dept=e_dept
     #Defining Identification of Employee
     def identify(self):
-        return f'Employee with Employee ID {self.e_id},is{self.e_name},and  working in the department:  {self.e_dept}'
-#employee1
-emp1=Employee()
-#employee 2
-emp2=Employee()
-emp1.e_id=1
-emp1.e_name='Rajesh'
-emp1.e_dept='Testing'
-emp2.e_id=2
-emp2.e_name='Saurav'
-emp2.e_dept='Designing'
-res=Employee.identify(emp1)
-print(res)
-res1=Employee.identify(emp2)
-print(res1)
+        return f'Employee with Employee ID {self.e_id} is{self.e_name} and  working in the department:  {self.e_dept}'
 
+if __name__=='__main__':
+    #employee1
+    emp1=Employee(1,"Rajesh","Testing")
+    #employee 2
+    emp2=Employee(2,"Saurav","Designing")
+    print(Employee.identify(emp1))
+    print(Employee.identify(emp2))
 print("----------------------------------------------------------------------------------------------------------")
 
 
 #Class3
 class Calc:
-    length=0
-    breadth=0
-    height=0
+    def __init__(self,length,breadth,height):
+        self.length=length
+        self.breadth=breadth
+        self.height=height
     def volume(self):
         return self.length*self.breadth*self.height
 
-cal=Calc()
-cal.length=10
-cal.breadth=20
-cal.height=30
-print("Length: ",cal.length)
-print("Breadth: ",cal.breadth)
-print("Height: ",cal.height)
-print("Volume: ")
-res= cal.volume()
-print(res)
+if __name__=='__main__':
+    obj1=Calc(10,20,30)
+    obj2=Calc(20,30,40)
+
+    print("Volume: ")
+    print(Calc.volume(obj1))
+    print(Calc.volume(obj2))
 print("-----------------------------------------------------------------------------------------------------------")
 
 #Class4
 class Channel:
-    time= ''
-    name=""
-    anchors=""
+    
+    def __init__(self,time,name,anchors):
+        self.time=time
+        self.name=name
+        self.anchors=anchors
     #display channel information
     def disp_news(self):
         return f'The name of Channel is: {self.name} and it broadcasts at: {self.time} AM with anchor {self.anchors}'
+if __name__=='__main__':
+    ch1 = Channel(9,"Metro News One","Robin")
+    ch2=Channel(11,"Metro News One","Dony")
+    res = ch1.disp_news()
+    print(res)
+    res1=ch2.disp_news()
+    print(res1)
 
-ch1 = Channel()
-ch1.time=9
-ch1.name= "Metro News One"
-ch1.anchors="Robin"
-res = ch1.disp_news()
-print(res)
 
-
-print("-------------------------------------------------------------------------------------------------------")
+print("---------------------------------------------------------------------------------------------------------------")
 
 
 #Class5
 class Room:
-    length = 0.0
-    breadth = 0.0
-    
+    def __init__(self,length,breadth):
+        self.length=length
+        self.breadth=breadth
     #calculate area
     def calculate_area(self):
         return self.length*self.breadth
 
+if __name__=='__main__':
+    study_room = Room(35,40)
+    study_room2 = Room(45,80)
 
-study_room = Room()
-study_room.length = 82.5
-study_room.breadth = 79.2
-print("Length: ",study_room.length)
-print("Breadth: ",study_room.breadth)
+    print("Length for first room: ",study_room.length)
+    print("Breadth for first room: ",study_room.breadth)
 
-print("Area: ",study_room.calculate_area())
+    print("Area: ",study_room.calculate_area())
+
+    print("Length for second room: ",study_room2.length)
+    print("Breadth for second room: ",study_room2.breadth)
+    print("Area: ",study_room2.calculate_area())
 
 
 print("-----------------------------------------------------------------------------------------------------------------")
