0a456b0167fea519d4a7d02cf25be87685375234
rohitashav01 <rohitashav.pathak@netsolutions.com>
2023-02-21 10:18:49 +0530

    Singleton Class Assignment

---
+++ singleton_class.py
@@ -0,0 +1,19 @@
+class Employee:
+    _name = None
+
+    def __new__(cls):
+        if cls._name is None:
+            print("Creating object using new")
+            cls._name = super().__new__(cls)
+            return cls._name
+        else:
+            raise Exception('class can have only one object')
+
+
+
+obj1=Employee()
+print(obj1)
+
+
+#obj2=Employee()
+#print(obj2)
