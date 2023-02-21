a4d4c8b49329157c07da9acaa1e16cb7944fd53e
rohitashav01 <rohitashav.pathak@netsolutions.com>
2023-02-21 13:41:00 +0530

    Change in Design Pattern

--- design_pattern.py
+++ design_pattern.py
@@ -1,22 +1,22 @@
 
 class Mercedes:
-    def  __init__(self,model):
-        self.model=model
+    def  __init__(self):
+        self.model = None
     
     def Price(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Price: 35Lacs"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Price: 50Lacs"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Price: 70Lacs"
 
     def mileage(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Mileage:15Kmpl"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Mileage:12Kmpl"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Mileage:10Kmpl"
 
     def interior_sound(self):
@@ -25,24 +25,24 @@ class Mercedes:
     def thermal_mgmt(self):
         return "Automatic Thermal Management"
 
-class Bmw:
-    def  __init__(self,model):
-        self.model=model
+class Bmw: 
+    def  __init__(self):
+        self.model = None
     
     def Price(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Price: 40Lacs"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Price: 60Lacs"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Price: 90Lacs"
 
     def mileage(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Mileage:14Kmpl"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Mileage:11Kmpl"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Mileage:9Kmpl"
 
     def interior_sound(self):
@@ -53,23 +53,23 @@ class Bmw:
     
 
 class Porsche:
-    def  __init__(self,model):
-        self.model=model
+    def  __init__(self):
+        self.model=None
     
     def Price(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Price: 40Lacs"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Price: 65Lacs"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Price: 95Lacs"
 
     def mileage(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Mileage:13Kmpl"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Mileage:11Kmpl"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Mileage:7Kmpl"
 
     def interior_sound(self):
@@ -80,24 +80,23 @@ class Porsche:
     
 
 class Bentley:
-
-    def  __init__(self,model):
-        self.model=model
+    def __init__(self):
+        self.model=None
     
     def Price(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Price: 35Lacs"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Price: 55Lacs"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Price: 78Lacs"
 
     def mileage(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Mileage:17Kmpl"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Mileage:13Kmpl"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Mileage:12Kmpl"
 
     def interior_sound(self):
@@ -107,24 +106,24 @@ class Bentley:
         return "Thermal Management uses refrigerator circuits and coolant"
     
 
-class Chrysler:
-    def  __init__(self,model):
-        self.model=model
+class Chrysler: 
+    def __init__(self):
+        self.model= None
     
     def Price(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Price: 30Lacs"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Price: 45Lacs"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Price: 80Lacs"
 
     def mileage(self):
-        if self.model=="Base":
+        if self.model=="Base".lower():
             return "Mileage:15Kmpl"
-        if self.model=="Middle":
+        if self.model=="Middle".lower():
             return "Mileage:12Kmpl"
-        if self.model=="Top":
+        if self.model=="Top".lower():
             return "Mileage:10Kmpl"
 
     def interior_sound(self):
@@ -134,23 +133,31 @@ class Chrysler:
         return "Thermal Management uses coolant"
     
     
-def Factory(cars,model):
-    # return eval(headphone)()
+def Factory(cars):
 
     cls_dict = {
-        "mercedes":Mercedes(model),
-        "bmw":Bmw(model),
-        "porsche":Porsche(model),
-        "bentley":Bentley(model),
-        "chrysler":Chrysler(model)
+        "mercedes":Mercedes(),
+        "bmw":Bmw(),
+        "porsche":Porsche(),
+        "bentley":Bentley(),
+        "chrysler":Chrysler()
     }
     return cls_dict[cars.lower()]
 
 
-car_lst=[Mercedes,Bmw,Porsche,Bentley,Chrysler]
+car_lst=["mercedes","bmw","porsche","bentley","chrysler"]
 for x in car_lst:
-    print("Price for Base Model:{} and Mileage:".format(x('Base').Price()),x('Base').mileage())
-    print("Price for Middle Model:{} and Mileage:".format(x('Middle').Price()),x('Middle').mileage())
-    print("Price for Top Model:{} and Mileage:".format(x('Top').Price()),x('Base').mileage())
+    obj=Factory(x)
+    print("---------------------------Base Models of All Brands---------------------------------------------")
+    obj.model="base"
+    print("Price for Base Model:{} and Mileage:{}".format(obj.Price(),obj.mileage()))
+
+    print("---------------------------Mid Models of All Brands----------------------------------------------")
+    obj.model="middle"
+    print("Price for Middle Model:{} and Mileage:{}".format(obj.Price(),obj.mileage()))
+
+    print("---------------------------Top Models of All Brands----------------------------------------------")
+    obj.model="top"
+    print("Price for Top Model:{} and Mileage:{}".format(obj.Price(),obj.mileage()))
   
 
